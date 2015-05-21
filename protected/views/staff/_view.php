5f71259c9f17b80c640d3457826d33ddb3bd2d8f
Mashpy <mashpysays@gmail.com>
2015-05-21 08:08:49 +0000

    added staff controller using crud

---
+++ protected/views/staff/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this StaffController */
+/* @var $data Staff */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('designation')); ?>:</b>
+	<?php echo CHtml::encode($data->designation); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('role')); ?>:</b>
+	<?php echo CHtml::encode($data->role); ?>
+	<br />
+
+
+</div>
